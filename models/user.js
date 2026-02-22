834e8bbe0db8bdd60898e41dbff9b5c5aaeae258
Hardeep Singh <hardeepmalhotra022@gmail.com>
2026-02-22 21:47:46 +0530

    Improve user schema email validation
    Enhanced email field validation with unique constraint and error messages.

--- models/user.js
+++ models/user.js
@@ -1,20 +1,42 @@
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+// const passportLocalMongoose = require("passport-local-mongoose");
+// const { estimatedDocumentCount } = require("./review");
+
+
+// const userSchema = new Schema({
+//     email: {
+//         type: String,
+//         required: true,
+       
+
+//     }
+// })
+
+// userSchema.plugin(passportLocalMongoose);
+
+// module.exports = mongoose.model('User', userSchema);
+
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
-const { estimatedDocumentCount } = require("./review");
-
 
 const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-       
-
-    }
-})
+  email: {
+    type: String,
+    required: [true, "Email is required"], // Error message agar empty ho
+    unique: true, // Ek email se ek hi account
+    lowercase: true, // Email hamesha small letters mein save hoga
+    trim: true, // Faltu ki spaces (aage/piche) hata dega
+    // Ye Regex (Regular Expression) check karega ki format sahi hai ya nahi
+    match: [
+      /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+      "Please fill a valid email address",
+    ],
+  },
+});
 
 userSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model('User', userSchema);
-
+module.exports = mongoose.model("User", userSchema);
 
